fb2a293fd4f1804578f9aba9635412d82a322801
Toby J. Thorne <toby@tobythorne.com>
2021-01-12 11:10:36 -0500

    Tidy and create vignette file

--- R/batr.R
+++ R/batr.R
@@ -1,11 +1,10 @@
 #' batr: Imports GUANO Annotated Files and Creates Summary Data and Plots.
 #'
-#' A collection of tools to import and use GUANO metadata in annotated WAV
-#' files of bat recordings. The package depends on comprehensive metadata being
+#' A collection of tools to import and use GUANO metadata in annotated WAV files
+#' of bat recordings. The package depends on comprehensive metadata being
 #' present in the files. The package extracts this data to a text file for
 #' future loading. It calculates summary data and basic plots.
 #'
-#' @section Foo functions: The foo functions ...
 #'
 #' @docType package
 #' @name batr
