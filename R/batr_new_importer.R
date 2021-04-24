816cdd0c541ebf08428b435e2e488daca8a5e2f1
Toby J. Thorne <toby@tobythorne.com>
2021-04-24 18:50:31 -0400

    Rewrite read_wavs, and fix "new" and "add" helper functions

--- R/batr_new_importer.R
+++ R/batr_new_importer.R
@@ -25,36 +25,29 @@
 #' GUANO.reader("C:/Folder/Folder/File_Folder", "Project_NA")
 #'}
 #'@export
-read_wavs <- function(action = NULL, input_path = NULL, data_path = NULL) { #, verbose_observations = F, species_list = c("Epfu", "Labo", "Laci", "Lano", "Myle", "Mylu", "Myse", "Mysp", "Pesu")) {
+read_wavs <- function(action, input_path, site_col, data_path = NULL) {
 
-  if (is.null(action)) {
-    stop("No action given, please specifiy an action (\"New\", \"Add\" or \"Update\") and try again. See ?read_wavs for more information")
-  } else if (action == "New") {
-    .new_observations(input_path, data_path)
+    if (action == "New") {
+      if (!is.null(data_path)) {
+      overwrite <- readline(prompt="Overwrite existing data file? Y/N: ")
+        if (overwrite == "Y" | overwrite == "y") {
+        .new_observations(input_path, site_col, data_path)
+      }
+    } else {
+      .new_observations(input_path, site_col, data_path)
+    }
   } else if (action == "Add") {
-    .add_observations(input_path, data_path)
+    .add_observations(input_path, data_path, species_list, site_col)
   } else if (action == "Update") {
-    .update_observations()
+    .update_observations(input_path, data_path, species_list)
   } else {
     stop("Invalid action given, please specifiy an action (\"New\", \"Add\" or \"Update\") and try again. See ?read_wavs for more information")
   }
 }
 
-#'New Observations
-#'
-#'Reads file GUANO and saves them into an RData file. 
-#'
-#'@family Import Functions
-#'
-#'@param path Path to dataset.
-#'
-#'@return A data frame of observations in the environment and a new
-#'  observations.csv saved at target folder.
-.new_observations <- function(input_path, data_path) {
-  file_list <- .get_file_list(input_path)
-  observations <- do.call(plyr::rbind.fill, (lapply(pbapply::pblapply(as.list(file_list$Full.Path), read.guano), as.data.frame)))
-  observations <- merge(observations, file_list, by.x = "File.Name", by.y = "File.Name")
-  observations <- observations[, which(names(observations) != "Full.Path")]
+.new_observations <- function(input_path, site_col, data_path) {
+  file_list <- .get_file_list(input_path) # Get list of files
+  observations <- .read_file_GUANO(file_list, species_list, site_col) # Read GUANO
   if (is.null(data_path)) {
     save_path <- readline(prompt="No 'data_path' specified. Please input path to save data file (e.g. C/user/directory):")
     if (!dir.exists(save_path)) {
@@ -66,69 +59,61 @@ read_wavs <- function(action = NULL, input_path = NULL, data_path = NULL) { #, v
       # assign("observations", observations, envir=globalenv())
     }
   } else if (!is.null(data_path)) {
-    save(observations, file = data_path)
-  }
+    save(observations, file = data_path) 
+  } # FIXME Save to data file
 }
-  
-#'Update Observations
-#'
-#'Compares directory files to existing dataset and updates as required.
-#'
-#'@family Import Functions
-#'
-#'@param path Path to dataset. 
-#'
-#'@return A data frame of observations in the environment.
+
+.add_observations <- function(input_path, data_path, species_list, site_col) {
+  load(data_path) # Load current data
+  observations_original <- observations # Rename current observations for clarity
+  rm(observations) # Remove current observations df for clarity
+  observations_new <- .get_file_list(input_path) # Get list observations in input director
+  observations_new <- observations_new[!(observations_new$File.Name %in% observations_original$File.Name),] # Remove directory observations already present in current data
+  observations_new2 <- .read_file_GUANO(observations_new, species_list = species_list, site_col = site_col) # Read GUANO for new files
+  observations_new <- merge(observations_new2, observations_new, by.x = "File.Name", by.y = "File.Name") # Combine GUANO and file metadata
+  observations_new <- observations_new[, which(names(observations_new) != "Full.Path")] # Remove duplicate column
+  observations_original[setdiff(names(observations_new), names(observations_original))] <- NA # Prepare for column differences in tables
+  observations_new[setdiff(names(observations_original), names(observations_new))] <- NA
+  observations <- rbind(observations_original, observations_new) # Bind original and new data
+  observations <- observations[order(observations$File.Name),] # Sort
+  rownames(observations) <- NULL # Reset row names
+  save(observations, file = data_path) # FIXME Save to datafile
+}
+
 .update_observations <- function(input_path, data_path) {
-  
-  # Get list of .wav files and file modification dates:
-  
-  directory_files <- .get_file_list(input_path)
-  
-  # Get list of file names and modification dates from the observations table:
-  
-  load(data_path)
-  observations_files <- observations[, c("File.Name", "File.Path", "File.Modified")]
-  names(observations_files)[1:3] <- c("File.Name","Observations.Path", "Observations.Modified")
-  
-  # Merge the two lists
-  
-  files_comparison <- merge(directory_files, observations_files, by = "File.Name", all = T)
-  
-  # Identify files to update in dataset
+  directory_files <- .get_file_list(input_path) # Get input file list
+  load(data_path) # Load existing data
+  observations_files <- observations[, c("File.Name", "File.Path", "File.Modified")] # Trim existing data to relevant columns
+  names(observations_files)[1:3] <- c("File.Name","Observations.Path", "Observations.Modified") # Rename existing data columns for clarify
+  files_comparison <- merge(directory_files, observations_files, by = "File.Name", all = T) # Merge new and existing data frames by filename
   files_comparison$update <- ifelse(mapply(function(x, y) {isTRUE(all.equal(x, y))}, files_comparison$Directory.Modified, files_comparison$Observations.Modified), "N", 
-                                    ifelse(files_comparison$Directory.Modified > files_comparison$Observations.Modified, "Y", "N"))
+                                    ifelse(files_comparison$Directory.Modified > files_comparison$Observations.Modified, "Y", "N")) # Mark files where modified dates don't match, and input file version is newer
+  update_files <- subset(files_comparison, update == "Y") # Subset files to update
+  update_files <- .get_file_list(update_files)
   
-  # Read GUNANO from updated files
-  update_files <- subset(files_comparison, update == "Y")
-  update_files <- update_files$Directory.Path
-  update_files <- cbind(file=update_files,
-                        dir=dirname(update_files),
-                        data.frame(file.info(update_files), row.names =NULL),
-                        stringsAsFactors=FALSE)
-  update_files$filename <- sub('.*\\/', '', update_files$file)
-  update_files <- update_files[, c("filename", 'file', "mtime")]
-  names(update_files)[1:3] <- c("File.Name", "Full.Path", "File.Modified")
-  update_files$File.Modified <- as.numeric(update_files$File.Modified)
-  observations_update <- do.call(plyr::rbind.fill, (lapply(pbapply::pblapply(as.list(update_files$Full.Path), read.guano), as.data.frame)))
-  observations_update <- merge(observations_update, update_files, by.x = "File.Name", by.y = "File.Name")
-  observations_update <- observations_update[, which(names(observations_update) != "Full.Path")]
+
+  #update_files <- update_files$Directory.Path
+  #update_files <- cbind(file=update_files,
+  #                      dir=dirname(update_files),
+  #                      data.frame(file.info(update_files), row.names =NULL),
+  #                      stringsAsFactors=FALSE)
+  #update_files$filename <- sub('.*\\/', '', update_files$file)
+  #update_files <- update_files[, c("filename", 'file', "mtime")]
+  #names(update_files)[1:3] <- c("File.Name", "Full.Path", "File.Modified")
+  #update_files$File.Modified <- as.numeric(update_files$File.Modified)
+
+  observations_update <- .read_file_GUANO()
   
-  # Remove rows to update from dataset
-  observations_original <- observations[!(observations$File.Name %in% observations_update$File.Name),]
+  #observations_update <- do.call(plyr::rbind.fill, (lapply(pbapply::pblapply(as.list(update_files$Full.Path), read.guano), as.data.frame)))
+  #observations_update <- merge(observations_update, update_files, by.x = "File.Name", by.y = "File.Name")
+  #observations_update <- observations_update[, which(names(observations_update) != "Full.Path")]# Remove unecessary column
   
-  # Account for differences in columns
-  observations_files[setdiff(names(observations_update), names(observations_original))] <- NA
-  observations_update[setdiff(names(observations_original), names(observations_update))] <- NA
-  
-  # Bind original and updated rows, set order by filename and reset rownames
-  observations <- rbind(observations_original, observations_update)
-  observations <- observations[order(observations$File.Name),]
-  rownames(observations) <- NULL
-  
-  # Assign and save!
-  #assign("observations", observations, envir=globalenv())
-  #save(observations, file = paste(path, "/BAM_Data.RData", sep = ""))  
+  observations_original <- observations[!(observations$File.Name %in% observations_update$File.Name),] # Remove updated rows from original data
+  observations_files[setdiff(names(observations_update), names(observations_original))] <- NA # Account for differences in columns
+  observations_update[setdiff(names(observations_original), names(observations_update))] <- NA # ^
+  observations <- rbind(observations_original, observations_update) # Bind new and updated rows
+  observations <- observations[order(observations$File.Name),] # Reorder by filename
+  rownames(observations) <- NULL # Reset row names
   if (is.null(data_path)) {
     save_path <- readline(prompt="No 'data_path' specified. Please input path to save data file (e.g. C/user/directory):")
     if (!dir.exists(save_path)) {
@@ -137,29 +122,9 @@ read_wavs <- function(action = NULL, input_path = NULL, data_path = NULL) { #, v
       filename <- readline(prompt="Please input your desired filename:")
       data_path <- paste(save_path, "/", filename, ".RData", sep = "")
       save(observations, file = data_path)
-      # assign("observations", observations, envir=globalenv())
     }
-  }
+  } # FIXME Save to data file
 }  
-
-.add_observations <- function(input_path, data_path) {
-  load(data_path) # Load current data
-  observations_original <- observations # Rename current observations for clarity
-  rm(observations) # Remove current observations df for clarity
-  observations_new <- .get_file_list(input_path) # Get list observations in input director
-  observations_new <- observations_new[!(observations_new$File.Name %in% observations_original$File.Name),] # Remove directory observations already present in current data
-  
-  observations_new2 <- do.call(plyr::rbind.fill, (lapply(pbapply::pblapply(as.list(observations_new$Full.Path), read.guano), as.data.frame)))
-  observations_new <- merge(observations_new2, observations_new, by.x = "File.Name", by.y = "File.Name")
-  observations_new <- observations_new[, which(names(observations_new) != "Full.Path")]
-  
-  observations_original[setdiff(names(observations_new), names(observations_original))] <- NA
-  observations_new[setdiff(names(observations_original), names(observations_new))] <- NA
-  observations <- rbind(observations_original, observations_new)
-  observations <- observations[order(observations$File.Name),]
-  rownames(observations) <- NULL
-  save(observations, file = data_path)
-}
 
 .get_file_list <- function(input_path) {
   file_list <- list.files(input_path, full.names = TRUE, recursive = TRUE, pattern = ".wav")
@@ -172,4 +137,48 @@ read_wavs <- function(action = NULL, input_path = NULL, data_path = NULL) { #, v
   names(file_list)[1:3] <- c("File.Name", "Full.Path", "File.Modified")
   file_list$File.Modified <- as.numeric(file_list$File.Modified)
   return(file_list)
+}
+
+.read_file_GUANO <- function(file_list, species_list, site_col) {
+  
+  observations <- do.call(plyr::rbind.fill, (lapply(pbapply::pblapply(as.list(file_list$Full.Path), read.guano), as.data.frame))) # Read GUANO
+  observations <- merge(observations, file_list, by.x = "File.Name", by.y = "File.Name") # Add file modified columns to GUNAO df
+  observations <- observations[ , !names(observations) %in% c("Full.Path","Anabat.Signature")] # Remove undesired columns
+  
+  observations <- within(observations, {
+    Night = ifelse(lubridate::hour(observations$Timestamp) > 11, 
+                   as.Date(observations$Timestamp, tz = "EST"), 
+                   as.Date(observations$Timestamp, tz = "EST") - 1)
+  }) # Generate Night column, accounting for observations after midnight
+  observations$Night <- as.Date(observations$Night, origin = "1970-01-01") # Set format of date column
+  
+  observations = within(observations, {
+    Species = ifelse(is.na(observations$Species.Manual.ID), as.character(observations$Species.Auto.ID), as.character(observations$Species.Manual.ID))
+  }) # Generate Species column
+  
+  # observations <- observations[observations$Species %in% species_list, ] # Remove rows without species in species list
+  
+  colnames(observations)[colnames(observations)=="Loc.Position.Lat"] <- "Latitude"
+  colnames(observations)[colnames(observations)=="Loc.Position.Lon"] <- "Longitude"
+  colnames(observations)[colnames(observations)==site_col] <- "Location" # Set location column
+    
+  swift_files <- observations[observations$Model == 'Swift',] # Subset files recorded with Anabat Swift
+  location_list <- unique(swift_files$Location) # Find locations
+  for (Location in location_list) {
+    location_subset           <- swift_files[swift_files$Location == Location,] # Subset once more by location
+    location_subset$Latitude  <- location_subset[1,"Latitude"] # Replace all lat values with first instance
+    location_subset$Longitude <- location_subset[1,"Longitude"] # Replace all lon values with first instance
+    if (!exists("swift_files_mod")) {
+      swift_files_mod <- location_subset
+    } else {
+      swift_files_mod <- rbind(swift_files_mod, location_subset)
+    } # Create modified observations.frame if it doesn't exist, or apend if it does
+  }
+  observations <- observations[!(observations$Model == 'Swift'),] # Remove modified rows
+  if (exists("swift_files_mod")) {
+    observations <- rbind(observations, swift_files_mod) # Replace removed rows with modified rows
+  }
+  
+  observations <- dplyr::select(observations, Timestamp, Species, Location, Latitude, Longitude, Species.Auto.ID, Species.Manual.ID, Night, File.Name, everything())
+  return(observations)
 }
