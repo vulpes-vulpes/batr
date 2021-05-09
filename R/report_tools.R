eaa6f242ac022ba06226abe620ae29b07ef4e247
Toby J. Thorne <toby@tobythorne.com>
2021-05-09 13:00:25 -0400

    Add initial files for multi-site report

--- R/report_tools.R
+++ R/report_tools.R
@@ -4,6 +4,12 @@ one_site_short_report <- function(data_path, project_name, species_list) {
                     params = list(project = project_name, data = data_path))
 }
 
+multi_site_short_report <- function(data_path, project_name, species_list) {
+  rmarkdown::render(input = system.file("rmd", "One_Site_Short_Report.Rmd", package = "batr"), 
+                    output_file = paste0("/Users/toby/Documents/0 - Inbox/Bat_Test_Dataset/Test_" , project_name, ".pdf"),
+                    params = list(project = project_name, data = data_path))
+}
+
 .map4report <- function(observations) {
   locations <- as.data.frame(cbind(unique(observations$Longitude), unique(observations$Latitude)))
   colnames(locations) <- c("Longitude","Latitude")
