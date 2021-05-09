4c227046188683c493102a18d5d928b8bfd0b735
Toby J. Thorne <toby@tobythorne.com>
2021-05-09 18:23:16 -0400

    Add quick summary table function and update documentation

--- R/report_tools.R
+++ R/report_tools.R
@@ -25,3 +25,33 @@ multi_site_short_report <- function(data_path, project_name, species_list) {
   return(map)
 }
 
+#'Create quick summary table
+#'
+#'@importFrom data.table %chin%
+#'
+#'@param observations Observations data frame. 
+#'@param active_dates Optional active_dates data frame
+#'
+#'@return A quick summary table of total species observations and survey effort.
+#'
+.quick_summary <- function (observations, active_dates = NULL) {
+  observations_s <- data.table::setDT(observations)[Species %chin% species]
+  observations_s$Loc_Year <- paste(lubridate::year(observations_s$Night), observations_s$Location, sep = " ")
+  quick_summary <- as.data.frame.matrix(table(observations_s$Loc_Year, observations_s$Species)) # Create table of Species by Location
+  quick_summary <- cbind(Location = rownames(quick_summary), quick_summary) # Fix row names to column
+  rownames(quick_summary) <- NULL # with above
+  headers <- c("Location", species)
+  missing <- setdiff(headers, names(quick_summary)) 
+  quick_summary[missing] <- 0                    
+  quick_summary <- quick_summary[headers]
+  quick_summary$Year = substr(quick_summary$Location, 1, 4)
+  quick_summary$Location <- substring(quick_summary$Location, 6, 1000000L)
+  quick_summary$Monitoring_Nights <- sapply(quick_summary$Location, function(l) nrow(active_dates[active_dates$Location==l,]))
+  
+  #div <- sapply(quick_summary$Location, function(l) nrow(active_dates[active_dates$Location==l,])) # , quick_summary$Location)
+  #div <- as.data.frame(div)
+  
+  #averages <- as.data.frame(mapply('/', quick_summary[species], div))
+  
+  return(quick_summary)
+}
