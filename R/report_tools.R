cc0a063d13e9847b14b3f4a4b7b0883156136487
Toby J. Thorne <toby@tobythorne.com>
2021-05-09 16:36:06 -0400

    Add label function to map4report

--- R/report_tools.R
+++ R/report_tools.R
@@ -10,9 +10,18 @@ multi_site_short_report <- function(data_path, project_name, species_list) {
                     params = list(project = project_name, data = data_path))
 }
 
-.map4report <- function(observations) {
-  locations <- as.data.frame(cbind(unique(observations$Longitude), unique(observations$Latitude)))
-  colnames(locations) <- c("Longitude","Latitude")
-  map <- ggmap::qmplot(Longitude, Latitude, data = locations, maptype = "toner-lite", extent = "normal")
+.map4report <- function(observations, labels = T) {
+  locations <- as.data.frame(cbind(unique(observations$Location), unique(observations$Longitude), unique(observations$Latitude)))
+  colnames(locations) <- c("Location", "Longitude","Latitude")
+  locations$Latitude <- as.numeric(locations$Latitude)
+  locations$Longitude <- as.numeric(locations$Longitude) 
+   
+  map <- ggmap::qmplot(x = Longitude, y = Latitude, data = locations, maptype = "toner-lite", extent = "normal") + 
+         if (labels == T) {   
+            ggrepel::geom_text_repel(data = locations,
+             ggplot2::aes(label = Location),
+             size=3)
+         }
   return(map)
 }
+
