c8d7c59e4d6c539ed20007eddec472bb7ce4d9e3
Toby J. Thorne <toby@tobythorne.com>
2023-02-17 16:12:59 -0500

    Adopt newer, more flexible, approach to manual vetting

--- R/new_manual_vetting.R
+++ R/new_manual_vetting.R
@@ -9,10 +9,7 @@
 #'@param data_path Character. Path to an existing RData file for the data set
 #'  you wish to manually vet.
 #'@param WAV_directory Character, the parent directory containing original WAV
-#'  files. \strong{Note: Right now the data within this directory must have the
-#'  same structure (i.e. exactly the same organisation of sub-directories) as
-#'  originally used to create the RData file. However, by specifying the parent
-#'  directory you can account for moving between systems.}
+#'  files.
 #'@param save_directory Character, the destination where selected files will be
 #'  copied to
 #'@param species_list Character, a single species or list in the form of four
@@ -34,18 +31,32 @@
 manual_vet_extractor <- function(data_path, WAV_directory, save_directory, species_list, percentage = 0.05, no_manual = FALSE) {
   .check_data_path(data_path)
   load(data_path)
-  dataset <- observations
+  dataset <- observations # rename for ease
   if(no_manual == TRUE) {
     dataset <- dataset[is.na(dataset$Species.Manual.ID),]
-  }
-  dataset <- dataset[!is.na(dataset$Species),]
+  } # drop observations with existing manual IDs if selected
+  dataset <- dataset[!is.na(dataset$Species),] # drop observations without speices
+  message("Matching observations to WAV files.")
+  WAV_directory_files <- .get_file_list(WAV_directory) # get file list from the directory of WAV files
+  dataset2 <- merge(dataset, WAV_directory_files, by = "File.Name", all.x = T) # merge the list of observations with the list of WAV files provided
+  if (sum(is.na(dataset2$Full.Path)) != 0) {
+    message("Error: cannot locate WAV files for some observations in the specified WAV_directory. 
+    Would you like to see a list of observations that cannot be located?")
+    report <- readline(prompt="y/n:")
+    if (report == "y" | report == "Y") {
+      missing_files <- dataset2[is.na(dataset2$Full.Path),]
+      missing_files <- missing_files$File.Name
+      message("Ending due to missing WAV files, no file have been copied for manual vetting.")
+      return(missing_files)
+    }
+    stop("Ending due to missing WAV files, no file have been copied for manual vetting.")
+  } # check if any WAV files are missing for the specified observations and offer a list if not
   for (i in species_list) {
-    if(!dir.exists(paste(save_directory,"/Five_Percent/",sep=""))) # Does 5% folder exist?
-      dir.create(paste(save_directory,"/Five_Percent/",sep="")) # If not then create it.
-    if(!dir.exists(paste(save_directory,"/Five_Percent/",i,sep=""))) # Does species folder exist within 5% folder?
-      dir.create(paste(save_directory,"/Five_Percent/",i,sep="")) # If not then create it.
-    temp_dataset <- dataset[ sample( which( dataset$Species == i ) , (sum(dataset$Species == i)*percentage) ) , ] # Create a temporary dataset with a random 5% of the rows for species.
-    file.copy(paste(WAV_directory,"/",temp_dataset$Local.Path,sep = ""),
-              paste(save_directory,"/Five_Percent/",i,sep="")) # Copy the five percent subset to the previously created species folder.
+    message(paste("Copying", i, "vetting files to output directory."))
+    if(!dir.exists(paste(save_directory,"/",i,sep=""))) # Does species folder exist within 5% folder?
+      dir.create(paste(save_directory,"/",i,sep="")) # If not then create it.
+    temp_dataset <- dataset2[ sample( which( dataset2$Species == i ) , (sum(dataset2$Species == i)*percentage) ) , ] # Create a temporary dataset with a random 5% of the rows for species.
+    file.copy(temp_dataset$Full.Path,
+              paste(save_directory,"/",i,sep="")) # Copy the five percent subset to the previously created species folder.
   }
 }
