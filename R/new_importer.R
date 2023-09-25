fcc302a7f22f46615984736207f68ca87fe56fbe
Toby J. Thorne <toby@tobythorne.com>
2023-09-25 15:38:23 -0400

    Fix faster file reading in Windows environments.

--- R/new_importer.R
+++ R/new_importer.R
@@ -116,8 +116,12 @@ import_GUANO <- function(action, input_path, site_col, timezone, data_path = NUL
     message("Making list of available WAV files.")
     if (fast_import == T) {
       message("Using fast file reading!")
-      file_list_full <- system(sprintf('find "%s" -name "*.wav"', input_path), intern=T) # Faster system call to get file list
-      file_list_short <- sub('.*\\/', '', file_list_full)
+      if (.Platform$OS.type == "unix") {
+        file_list_full <- system(sprintf('find "%s" -name "*.wav"', input_path), intern=T) # Faster system call to get file list
+      } else if (.Platform$OS.type == "windows") {
+        file_list_full <- shell(sprintf('dir /s /b "%s\\*.wav"', input_path), intern=T) # System call for Windows environments
+      }
+    file_list_short <- sub('.*\\/', '', file_list_full)
     } else {
       message("Using slow file reading :(")
       file_list_full <- list.files(input_path, full.names = TRUE, recursive = TRUE, pattern = ".wav")
