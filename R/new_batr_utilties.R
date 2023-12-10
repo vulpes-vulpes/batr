ef60279dde519c0694c9175d739aa0f1d35039f0
Toby J. Thorne <toby@tobythorne.com>
2023-12-10 18:34:33 -0500

    Finish gap function for plot.

--- R/new_batr_utilties.R
+++ R/new_batr_utilties.R
@@ -142,27 +142,26 @@
     row <- row + 1 # Iterate row
   } # While loop to iterate gap numbers
   maximum_fail <- max(failed_dates$Gaps) # Calculate number of gaps
-  activity <- failed_dates # Copy dates without recordings to a new data frame
-  activity$Date <- NULL # Remove unnecessary column
-  activity$Log_Count <- NULL # Remove unnecessary column
-  activity <- unique(activity) # Remove duplicate rows
+  gap_list <- failed_dates # Copy dates without recordings to a new data frame
+  gap_list$Date <- NULL # Remove unnecessary column
+  gap_list$Log_Count <- NULL # Remove unnecessary column
+  gap_list <- unique(gap_list) # Remove duplicate rows
   fail <- 1 # Create fail variable
-  activity$xmin <- 0 # Create empty column for xmax
+  gap_list$xmin <- 0 # Create empty column for xmax
   while (fail < (maximum_fail+1)) {
-    activity[fail,3] <- min(failed_dates[failed_dates$Gaps == fail,1])
+    gap_list[fail,3] <- min(failed_dates[failed_dates$Gaps == fail,1])
     fail <- fail + 1
   } # Compute end date for each break
-  activity$xmin <- as.Date(activity$xmin, origin = "1970-01-01") # Convert to date
+  gap_list$xmin <- as.Date(gap_list$xmin, origin = "1970-01-01") # Convert to date
   fail <- 1 # Reset fail variable
-  activity$xmax <- 0 # Create empty column for xmin
+  gap_list$xmax <- 0 # Create empty column for xmin
   while (fail < (maximum_fail+1)) {
-    activity[fail,4] <- max(failed_dates[failed_dates$Gaps == fail,1])
+    gap_list[fail,4] <- max(failed_dates[failed_dates$Gaps == fail,1])
     fail <- fail + 1
   } # Compute start date for each break
-  activity$xmax <- as.Date(activity$xmax, origin = "1970-01-01") # Convert to date
-  activity$ymax <- Inf
-  activity$ymin <- 0
-  activity$Gaps <- NULL
-  gap_list <- activity
+  gap_list$xmax <- as.Date(gap_list$xmax, origin = "1970-01-01") # Convert to date
+  gap_list$ymax <- Inf
+  gap_list$ymin <- 0
+  gap_list$Gaps <- NULL
   return(gap_list)
 }
